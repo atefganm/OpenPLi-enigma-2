dae9cdc81ddf7c726914d2aacd59a00806ae1685
atefganm <atef_anim_1974@yahoo.com>
2022-09-19 12:34:33 +0200

    Introduce HasHDMIinFHD

--- lib/python/Screens/Hotkey.py
+++ lib/python/Screens/Hotkey.py
@@ -11,10 +11,9 @@ from Plugins.Plugin import PluginDescriptor
 from Tools.BoundFunction import boundFunction
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS
 from ServiceReference import ServiceReference
-from enigma import eServiceReference
+from enigma import eServiceReference, getDesktop
 from Components.Pixmap import Pixmap
 from Components.Label import Label
-from boxbranding import getHaveHDMIinHD, getHaveHDMIinFHD
 import os
 
 
@@ -193,9 +192,9 @@ def getHotkeyFunctions():
 	hotkey.functions.append((_("Activate HbbTV (Redbutton)"), "Infobar/activateRedButton", "InfoBar"))
 	if SystemInfo["HasHDMIin"]:
 		hotkey.functions.append((_("Toggle HDMI In"), "Infobar/HDMIIn", "InfoBar"))
-	if getHaveHDMIinHD() == 'True' or getHaveHDMIinFHD() == 'True':
-                hotkey.functions.append((_("Toggle HDMI-In full screen"), "Infobar/HDMIInFull", "InfoBar"))
-                hotkey.functions.append((_("Toggle HDMI-In PiP"), "Infobar/HDMIInPiP", "InfoBar"))
+	if SystemInfo["HasHDMIinFHD"]:
+		hotkey.functions.append((_("Toggle HDMI-In full screen"), "Infobar/HDMIInFull", "InfoBar"))
+		hotkey.functions.append((_("Toggle HDMI-In PiP"), "Infobar/HDMIInPiP", "InfoBar"))
 	if SystemInfo["LcdLiveTV"]:
 		hotkey.functions.append((_("Toggle LCD LiveTV"), "Infobar/ToggleLCDLiveTV", "InfoBar"))
 	hotkey.functions.append((_("Toggle dashed flickering line for this service"), "Infobar/ToggleHideVBI", "InfoBar"))
--- lib/python/StartEnigma.py
+++ lib/python/StartEnigma.py
@@ -588,7 +588,8 @@ profile("LCD")
 import Components.Lcd
 Components.Lcd.InitLcd()
 
-if boxtype in ('dm7080', 'dm820', 'dm900', 'dm920', 'dreamone', 'dreamtwo'):
+from Tools.HardwareInfo import HardwareInfo
+if HardwareInfo().get_device_model() in ('dm7080', 'dm820', 'dm900', 'dm920', 'dreamone', 'dreamtwo'):
 	print("[StartEnigma] Read /proc/stb/hdmi-rx/0/hdmi_rx_monitor")
 	check = open("/proc/stb/hdmi-rx/0/hdmi_rx_monitor", "r").read()
 	if check.startswith("on"):
